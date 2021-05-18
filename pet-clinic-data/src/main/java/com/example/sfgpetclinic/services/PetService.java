36d4486c3070cb8875aa178d52d0a65e89aeb7c1
CALBA Antonin <jaxcrocsblancs@laposte.net>
2021-05-18 15:48:58 +0200

    Refractoring of Service

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/PetService.java
@@ -2,13 +2,7 @@ package com.example.sfgpetclinic.services;
 
 import com.example.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
+public interface PetService extends CrudService<Pet, Long>  {
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-    
 }
