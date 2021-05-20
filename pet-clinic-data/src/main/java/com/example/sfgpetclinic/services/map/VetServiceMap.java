676aa59001f74cccc287a2907c4a070274af4727
CALBA Antonin <jaxcrocsblancs@laposte.net>
2021-05-21 00:05:53 +0200

    Add Speciality to vet

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.example.sfgpetclinic.services.map;
 
+import com.example.sfgpetclinic.model.Speciality;
 import com.example.sfgpetclinic.model.Vet;
+import com.example.sfgpetclinic.services.SpecialityService;
 import com.example.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,18 +10,35 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
     }
-    
+
     @Override
     public Vet findByID(Long id) {
         return super.findById(id);
     }
-    
+
     @Override
     public Vet save(Vet object) {
+
+            if (object.getSpeciality().size() > 0) {
+                object.getSpeciality().forEach(speciality -> {
+                    if (speciality.getId() == null) {
+                        Speciality savedSpecialty = specialityService.save(speciality);
+                        speciality.setId(savedSpecialty.getId());
+                    }
+                });
+            }
+
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -16,16 +16,25 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final VisitService visitService;
+    private final SpecialityService specialityService;
 
-    public DataLoader(OwnerService ownerService, PetService petService, VetService vetService, PetTypeService petTypeService, VisitService visitService) {
+    public DataLoader(OwnerService ownerService, PetService petService, VetService vetService, PetTypeService petTypeService, VisitService visitService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.petService = petService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.visitService = visitService;
+        this.specialityService = specialityService;
     }
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
+        if (count==0){
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -33,13 +42,25 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiologie");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         owner1.setAddress("123 Brickerel");
         owner1.setCity("Miami");
         owner1.setTelephone("1231231234");
-
 
 
         Pet mikesPet = new Pet();
@@ -84,14 +105,15 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-        //vet1.getSpecialities().add(savedRadiology);
+        vet1.getSpeciality().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-        //vet2.getSpecialities().add(savedSurgery);
+        vet2.getSpeciality().add(savedSurgery);
+        vet2.getSpeciality().add(savedDentistry);
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
