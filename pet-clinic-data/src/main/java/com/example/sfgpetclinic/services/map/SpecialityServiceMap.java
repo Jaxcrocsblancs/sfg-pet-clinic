7e955868e4fa15592e3d79a9266adf4fb767959e
CALBA Antonin <jaxcrocsblancs@laposte.net>
2021-05-20 23:49:11 +0200

    Add Speciality

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package com.example.sfgpetclinic.services.map;
+
+import com.example.sfgpetclinic.model.Speciality;
+import com.example.sfgpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+    
+    @Override
+    public Speciality findByID(Long id) {
+        return super.findById(id);
+    }
+    
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+   
+}
