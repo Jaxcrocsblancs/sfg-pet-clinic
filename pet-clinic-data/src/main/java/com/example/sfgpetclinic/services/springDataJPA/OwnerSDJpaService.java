df4eccd139e864d09b22c907c257732d04dd66a1
CALBA Antonin <jaxcrocsblancs@laposte.net>
2021-05-21 19:04:17 +0200

    add spring data jpa service for owner

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/springDataJPA/OwnerSDJpaService.java
@@ -0,0 +1,59 @@
+package com.example.sfgpetclinic.services.springDataJPA;
+
+import com.example.sfgpetclinic.model.Owner;
+import com.example.sfgpetclinic.repositories.OwnerRepository;
+import com.example.sfgpetclinic.repositories.PetRepository;
+import com.example.sfgpetclinic.repositories.PetTypeRepository;
+import com.example.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    public Owner findByLastName(String lastname){
+        return ownerRepository.findByLastName(lastname);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findByID(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
